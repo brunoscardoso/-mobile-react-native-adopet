be27fc6d7cfe2ff4ca910937c4bdf934d5a1567a
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2020-06-14 16:06:14 -0300

    create dropdowns

--- src/pages/RegisterPet/index.tsx
+++ src/pages/RegisterPet/index.tsx
@@ -1,6 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-import {Container, Card, Title, Label, Dropdown} from './styles';
+import {
+  Container,
+  Card,
+  Title,
+  Label,
+  GroupContainer,
+  DropDownContainer,
+  DropDownStyled,
+  Dropdown,
+} from './styles';
 
 import Header from '../../components/Header';
 import Input from '../../components/Input';
@@ -11,14 +21,82 @@ interface petObject {
   gender: string;
 }
 
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
 const RegisterPet = () => {
   const [name, setName] = useState('');
+  const [size, setSize] = useState('');
+  const [gender, setGender] = useState('');
 
-  const [size, setSize] = useState([
+  const [ufs, setUfs] = useState<string[]>([]);
+
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === '0') {
+      return;
+    }
+
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`,
+      )
+      .then((response) => {
+        const cityName = response.data.map((city) => city.nome);
+        setCities(cityName);
+      });
+  }, [selectedUf]);
+
+  const [genders] = useState([
+    {label: 'Macho', value: 'Macho'},
+    {label: 'Fêmea', value: 'Fêmea'},
+  ]);
+
+  const [sizes] = useState([
     {label: 'Pequeno', value: 'Pequeno'},
     {label: 'Médio', value: 'Médio'},
     {label: 'Grande', value: 'Grande'},
   ]);
+
+  function handleName(value: string) {
+    setName(value);
+  }
+
+  function handleSizeDropdown(value: string) {
+    setSize(value);
+  }
+
+  function handleGenderDropdown(value: string) {
+    setGender(value);
+  }
+
+  function handleUfsDropdown(value: string) {
+    setSelectedUf(value);
+  }
+
+  function handleCitiesDropdown(value: string) {
+    setSelectedCity(value);
+  }
 
   return (
     <Container>
@@ -27,8 +105,57 @@ const RegisterPet = () => {
         <Title>Cadastro do Pet</Title>
         <Label>Nome (psiu, batisa ele ai)</Label>
         <Input />
-        <Label>Qual porte do pet?</Label>
-        <Dropdown onValueChange={(value) => console.log(value)} items={size} />
+        <GroupContainer>
+          <DropDownContainer>
+            <Label>Porte</Label>
+            <DropDownStyled>
+              <Dropdown
+                onValueChange={(value) => handleSizeDropdown(value)}
+                items={sizes}
+              />
+            </DropDownStyled>
+          </DropDownContainer>
+
+          <DropDownContainer>
+            <Label>Sexo</Label>
+            <DropDownStyled>
+              <Dropdown
+                onValueChange={(value) => handleGenderDropdown(value)}
+                items={genders}
+              />
+            </DropDownStyled>
+          </DropDownContainer>
+        </GroupContainer>
+
+        <GroupContainer>
+          <DropDownContainer>
+            <Label>Estado</Label>
+            <DropDownStyled>
+              <Dropdown
+                onValueChange={(value) => handleUfsDropdown(value)}
+                items={ufs.map((sigla) => ({
+                  key: sigla,
+                  label: sigla,
+                  value: sigla,
+                }))}
+              />
+            </DropDownStyled>
+          </DropDownContainer>
+
+          <DropDownContainer>
+            <Label>Cidade</Label>
+            <DropDownStyled>
+              <Dropdown
+                onValueChange={(value) => handleCitiesDropdown(value)}
+                items={cities.map((city) => ({
+                  key: city,
+                  label: city,
+                  value: city,
+                }))}
+              />
+            </DropDownStyled>
+          </DropDownContainer>
+        </GroupContainer>
       </Card>
     </Container>
   );
--- src/pages/RegisterPet/styles.tsx
+++ src/pages/RegisterPet/styles.tsx
@@ -20,11 +20,29 @@ export const Title = styled.Text`
 export const Label = styled.Text`
   color: #6c6c80;
   font-size: 16px;
-  margin-bottom: 8px;
+  margin: 5px 0 8px 0;
+`;
+
+export const GroupContainer = styled.View`
+  margin: 10px 0 0 0;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const DropDownContainer = styled.View`
+  justify-content: center;
+`;
+
+export const DropDownStyled = styled.View`
+  padding: 0 15px;
+  width: 180px;
+  height: 40px;
+  background: #f0f0f5;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Dropdown = styled(RNPickerSelect)`
-  height: 100%;
-  width: 100%;
   background: #6c6c80;
 `;
